1288b2aa932767a90072bd11c8c7360073f23452
Ethan Steinberg <ethanid@stanford.edu>
2023-06-17 14:51:41 +0000

    First pass

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pad
+
+Run the following scripts in order:
+
+create_labels.py
+create_features.py
+train_baselines.py
+train_motor.py
+
+eval_results.py
+
+Database path: /local-scratch/nigam/projects/ethanid/som-rit-phi-starr-prod.starr_omop_cdm5_deid_2023_02_08_extract_v8
+MOTOR path: /local-scratch/nigam/projects/ethanid/text_checks/motor_release
---
+++ create_features.py
@@ -0,0 +1,78 @@
+import argparse
+import datetime
+import os
+import pickle
+import json
+import csv
+import numpy as np
+from loguru import logger
+from typing import Any, Callable, List, Optional, Set, Tuple
+import collections
+
+import random
+import femr
+import femr.labelers
+import femr.featurizers
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run femr labeling")
+    parser.add_argument("--path_to_database", required=True, type=str, help="Path to femr database")
+    parser.add_argument("--path_to_output_dir", required=True, type=str, help="Path to save labeles")
+    parser.add_argument("--num_threads", type=int, help="The number of threads to use", default=1, )
+
+    args = parser.parse_args()
+    
+
+    PATH_TO_PATIENT_DATABASE = args.path_to_database
+    PATH_TO_OUTPUT_DIR = args.path_to_output_dir
+    NUM_THREADS: int = args.num_threads
+    
+    path_to_log_file: str = os.path.join(PATH_TO_OUTPUT_DIR, 'features_info.log')
+    if os.path.exists(path_to_log_file):
+        os.remove(path_to_log_file)
+    logger.add(path_to_log_file, level="INFO")  # connect logger to file
+    logger.info(f"Loading patient database from: {PATH_TO_PATIENT_DATABASE}")
+    logger.info(f"Saving output to: {PATH_TO_OUTPUT_DIR}")
+    logger.info(f"# of threads: {NUM_THREADS}")
+
+    labeled_patients = femr.labelers.load_labeled_patients(os.path.join(PATH_TO_OUTPUT_DIR, 'subsample_labeled_patients.csv'))
+
+    # Lets use both age and count featurizer
+    age = femr.featurizers.AgeFeaturizer()
+    count = femr.featurizers.CountFeaturizer(is_ontology_expansion=True)
+    featurizer_age_count = femr.featurizers.FeaturizerList([age, count])
+
+    # Preprocessing the featurizers, which includes processes such as normalizing age.
+    logger.info("Start | Preprocess featurizers")
+    featurizer_age_count.preprocess_featurizers(PATH_TO_PATIENT_DATABASE, labeled_patients, NUM_THREADS)
+    with open(os.path.join(PATH_TO_OUTPUT_DIR, 'featurizers.pkl'), 'wb') as f:
+        pickle.dump(featurizer_age_count, f)
+
+    logger.info("Finish | Preprocess featurizers")
+
+    logger.info("Start | Featurize patients")
+    results = featurizer_age_count.featurize(PATH_TO_PATIENT_DATABASE, labeled_patients, NUM_THREADS)
+    with open(os.path.join(PATH_TO_OUTPUT_DIR, 'features.pkl'), 'wb') as f:
+        pickle.dump(results, f)
+
+    logger.info("Finish | Featurize patients")
+    feature_matrix, patient_ids, label_values, label_times = (
+        results[0],
+        results[1],
+        results[2],
+        results[3],
+    )
+    label_set, counts_per_label = np.unique(label_values, return_counts=True)
+    logger.info("FeaturizedPatient stats:\n"
+                f"feature_matrix={repr(feature_matrix)}\n"
+                f"patient_ids={repr(patient_ids)}\n"
+                f"label_values={repr(label_values)}\n"
+                f"label_set={repr(label_set)}\n"
+                f"counts_per_label={repr(counts_per_label)}\n"
+                f"label_times={repr(label_times)}")
+    
+    
+    with open(os.path.join(PATH_TO_OUTPUT_DIR, "features_done.txt"), "w") as f:
+        f.write("done")
+    
+    logger.info("Done!")
